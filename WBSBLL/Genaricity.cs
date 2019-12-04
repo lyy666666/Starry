7ef501335206876ba2a689f179b0f9c6be4e87e4
星空吖 <@DESKTOP-PF3RFAF>
2019-12-04 19:44:24 +0800

    1

---
+++ WBSBLL/Genaricity.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace WBSBLL
+{
+    /// <summary>
+    /// T类型
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Genaricity<T>
+    {
+        /// <summary>
+        /// 反射查询分页
+        /// </summary>
+        /// <param name="name">查询Name</param>
+        /// <param name="pageIndex">分页启始值</param>
+        /// <param name="pageSize">分页每页显示页数</param>
+        /// <param name="sum">输出参数记录总数</param>
+        /// <returns></returns>
+        public abstract DataTable Select(string name, int pageIndex, int pageSize, int sum);
+        /// <summary>
+        /// 反射查询全部
+        /// </summary>
+        /// <param name="TypeName">数据库表名</param>
+        /// <returns></returns>
+        public static DataTable SelectAll(string TypeName)
+        {
+            T t = (T)Activator.CreateInstance(typeof(T));
+            string sql = $"select * from {TypeName}";
+            return DBHelper.ExecSqlDataTable(sql);
+        }
+        /// <summary>
+        /// 反射添加方法
+        /// </summary>
+        /// <param name="t">Model表</param>
+        /// <param name="TypeName">数据库表名</param>
+        /// <param name="key">主键</param>
+        /// <returns></returns>
+        public static int Add(T t, string TypeName, string key)
+        {
+            PropertyInfo[] processInfo = t.GetType().GetProperties();
+            string sql = $"insert into {TypeName} values(";
+            string sq = "";
+            foreach (PropertyInfo item in processInfo)
+            {
+                if (item.Name != key)
+                {
+ 	    if (item.GetValue(t) == null)
+                    {
+                        sq += "'" + "',";
+                    }
+                    else
+                    {
+                        sq += "'" + item.GetValue(t).ToString() + "',";
+                    }
+
+
+                }
+            }
+            sq = sq.Substring(0, sq.Length - 1);
+            //sq.TrimEnd();
+            sql += sq + ")";
+            return DBHelper.ExecSqlResult(sql);
+        }
+        /// <summary>
+        /// 反射修改方法
+        /// </summary>
+        /// <param name="nodel">Model表</param>
+        /// <param name="TypeName">数据库表名</param>
+        /// <param name="key">主键</param>
+        /// <param name="id">修改的ID</param>
+        /// <returns></returns>
+        public static int UpData(T nodel, string TypeName, string key, int id)// where T : new()
+        {
+            //获取Type对象，反射操作基本都是用Type进行的
+            Type type = typeof(T);
+            string strsql = string.Format("update {0} set ", TypeName);
+            //获取Type对象所有公共属性
+            PropertyInfo[] info = type.GetProperties();
+            string link = "";
+            foreach (PropertyInfo item in info)
+            {
+                if (item.Name != key)
+                {
+
+
+                    object val = item.GetValue(nodel);
+                    if (val != null)
+                    {
+                        //更新修改字段的值
+                        strsql += link + item.Name + "='" + val + "'";
+                        link = ",";
+                    }
+                }
+                
+            }
+            strsql += string.Format(" where {0}='{1}'", key, id);
+            //调用执行方法
+            return DBHelper.ExecSqlResult(strsql);
+
+        }
+        /// <summary>
+        /// 反射删除方法
+        /// </summary>
+        /// <param name="TypeName">数据库表名</param>
+        /// <param name="key">主键</param>
+        /// <param name="id">删除的ID</param>
+        /// <returns></returns>
+        public static int Delete(string TypeName, string key, string id)// where T : new()
+        {
+            //获取Type对象，反射操作基本都是用Type进行的
+            Type type = typeof(T);
+            string strsql = string.Format("delete from  {0} where {1}='{2}'", TypeName, key, id);
+            //调用执行方法
+            return DBHelper.ExecSqlResult(strsql);
+
+        }
+        /// <summary>
+        /// 反射查询一条
+        /// </summary>
+        /// <param name="TypeName">数据库表名</param>
+        /// <param name="key">主键</param>
+        /// <param name="id">查询的ID</param>
+        /// <returns></returns>
+        public static DataTable SelectOne(string TypeName, string key, int id)
+        {
+            T t = (T)Activator.CreateInstance(typeof(T));
+            string sql = $"select * from {TypeName} where {key}='{id}'";
+            return DBHelper.ExecSqlDataTable(sql);
+        }
+        /// <summary>
+        /// DataTable转List
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static List<T> DataTableToList(DataTable dt)
+        {
+            var lst = new List<T>();
+            var plist = new List<System.Reflection.PropertyInfo>(typeof(T).GetProperties());
+            foreach (DataRow item in dt.Rows)
+            {
+                T t = System.Activator.CreateInstance<T>();
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    PropertyInfo info = plist.Find(p => p.Name == dt.Columns[i].ColumnName);
+                    if (info != null)
+                    {
+                        if (!Convert.IsDBNull(item[i]))
+                        {
+                            info.SetValue(t, item[i], null);
+                        }
+                    }
+                }
+                lst.Add(t);
+            }
+            return lst;
+        }
+        /// <summary>
+        /// List转DataTable
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static DataTable ListToDataTable(IEnumerable<T> collection)
+        {
+            var props = typeof(T).GetProperties();
+            var dt = new DataTable();
+            dt.Columns.AddRange(props.Select(p => new DataColumn(p.Name, p.PropertyType)).ToArray());
+            if (collection.Count() > 0)
+            {
+                for (int i = 0; i < collection.Count(); i++)
+                {
+                    ArrayList tempList = new ArrayList();
+                    foreach (PropertyInfo pi in props)
+                    {
+                        object obj = pi.GetValue(collection.ElementAt(i), null);
+                        tempList.Add(obj);
+                    }
+                    object[] array = tempList.ToArray();
+                    dt.LoadDataRow(array, true);
+                }
+            }
+            return dt;
+        }
+    }
+}
---
+++ WBSDAL/DBHelpers.cs
@@ -0,0 +1,248 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBSDAL
+{
+    public class DBHelper
+    {
+        //连接数据库的字符串（可以通过工具栏-连接数据库获取/也可以通过配置web.config获取）
+        /// <summary>
+        /// 连接数据库字符串
+        /// </summary>
+        static string strConn = "Data Source=DESKTOP-PF3RFAF;Initial Catalog=NewsRelease;Integrated Security=True;pwd=456";
+
+        //web.config获取，先添加system.configuration.dll
+        //string _webconfig = System.Configuration.ConfigurationSettings.AppSettings["配置名称"];//会提示已过期
+        
+        //string _webconfig2 = System.Configuration.ConfigurationManager.AppSettings["配置名称"];
+        
+
+        /// <summary>
+        /// 执行添加、修改、删除的SQL语句，返回受影响行数
+        /// </summary>
+        /// <param name="strSql">SQL语句</param>
+        /// <returns>返回影响行数</returns>
+        public static int ExecSqlResult(string strSql)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(strSql, conn);
+                int result = command.ExecuteNonQuery();
+                conn.Close();
+                return result;
+            }
+        }
+
+
+        /// <summary>
+        /// 执行查询SQL语句，返回DataTable数据集
+        /// </summary>
+        /// <param name="strSql">SQL语句</param>
+        /// <returns>返回数据集DataTable</returns>
+        public static DataTable ExecSqlDataTable(string strSql)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+
+                SqlDataAdapter sqlData = new SqlDataAdapter(strSql, conn);
+                DataTable dt = new DataTable();
+                sqlData.Fill(dt);
+
+                //释放
+                sqlData.Dispose();
+
+                conn.Close();
+
+                return dt;
+            }
+        }
+
+        /// <summary>
+        /// 执行SQL语句，获取首行首列
+        /// </summary>
+        /// <param name="strSql">SQL语句</param>
+        /// <returns>返回首行首列</returns>
+        public static object ExecSqlScalar(string strSql)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(strSql);
+                object result = command.ExecuteScalar();
+                conn.Close();
+                return result;
+            }
+        }
+        #region DataTable转List集合
+
+        /// <summary>
+        /// DataTable转List集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static List<T> DataTableToList<T>(DataTable dt)
+        {
+            return JsonConvert.DeserializeObject<List<T>>(JsonConvert.SerializeObject(dt));
+        }
+
+        /// <summary>
+        /// 执行SqL语句，返回List集合
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="strSql"></param>
+        /// <returns></returns>
+        public static List<T> DataTableToList<T>(string strSql)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+
+                SqlDataAdapter sqlData = new SqlDataAdapter(strSql, conn);
+
+                DataTable dt = new DataTable();
+                sqlData.Fill(dt);
+                sqlData.Dispose();
+                conn.Close();
+
+                List<T> list = DataTableToList<T>(dt);
+
+                return list;
+            }
+        }
+        #endregion
+
+        #region 执行存储过程
+
+        /// <summary>
+        /// 执行存储过程，返回影响行数
+        /// </summary>
+        /// <param name="ProcName"></param>
+        /// <param name="Parms"></param>
+        /// <returns></returns>
+        public static int ExecProcGetResult(string ProcName, Dictionary<string, object> Parms)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.CommandText = ProcName;
+                command.CommandType = CommandType.StoredProcedure;
+	command.Connection = conn;
+                foreach (var item in Parms)
+                {
+                    SqlParameter parm = new SqlParameter(item.Key, item.Value);
+                    command.Parameters.Add(parm);
+                }
+                int result = command.ExecuteNonQuery();
+                conn.Close();
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 执行存储过程返回DataTable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="pairs"></param>
+        /// <returns></returns>
+        public static DataTable ExecSqlGetDataTable(string sql, Dictionary<string, object> pairs)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = sql;
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection = conn;
+                foreach (var item in pairs)
+                {
+                    command.Parameters.AddWithValue(item.Key, item.Value);
+                }
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                dataAdapter.Fill(dt);
+                dataAdapter.Dispose();
+                conn.Close();
+                return dt;
+            }
+        }
+
+
+
+
+
+        /// <summary>
+        /// 执行存储过程，获取数据集及输出参数
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ProcName"></param>
+        /// <param name="Parms"></param>
+        /// <param name="OutName">@totalcount输出参数</param>
+        /// <returns></returns>
+        public static List<T> ExecProcGetResult<T>(string ProcName, Dictionary<string, object> Parms, out string OutName)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+
+                OutName = "@totalcount";
+                conn.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = conn;
+
+                //设置该SQL语句为存储过程
+                command.CommandText = ProcName;
+                command.CommandType = CommandType.StoredProcedure;
+
+                //循环遍历添加参数
+                foreach (var item in Parms)
+                {
+                    SqlParameter parm = new SqlParameter();
+
+                    parm.ParameterName = item.Key;
+
+                    //判断是不是输出参数
+                    if (item.Key.ToLower().Equals(OutName))
+                    {
+                        parm.Direction = ParameterDirection.Output;
+                        parm.Size = 50;
+                    }
+                    else
+                    {
+                        parm.Value = item.Value;
+                    }
+                    command.Parameters.Add(parm);
+                }
+
+                //执行command，获取数据集
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                dataAdapter.Fill(dt);
+                dataAdapter.Dispose();
+
+                //返回输出参数
+                object o = command.Parameters[OutName].Value;
+                OutName = o.ToString();
+
+                //关闭连接
+                conn.Close();
+
+                return DataTableToList<T>(dt);
+            }
+        }
+
+        #endregion
+
+    }
+}
