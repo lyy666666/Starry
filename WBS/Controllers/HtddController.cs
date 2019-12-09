dc0345551406fc25438a4ff4b113e361634adaaa
李旺臣 <@DESKTOP-18HDOSJ>
2019-12-09 19:45:02 +0800

    1

--- WBS/Controllers/HtddController.cs
+++ WBS/Controllers/HtddController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.IO;
 
 namespace WBS.Controllers
 {
     public class HtddController : Controller
     {
         // GET: Htdd
-        public ActionResult Index()
+        #region 后台订单显示
+        public ActionResult Show()
         {
-            JavaScript;
+            string Url="";
+            HttpClient  http = new HttpClient();
+            HttpResponseMessage message = http.GetAsync(Url);
+            string sql = message.Content.ReadAsByteArrayAsync().Result;
+
             return View();
         }
+	    #endregion
+        
     }
 }
