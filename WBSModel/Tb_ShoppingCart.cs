1574537933b36d2f7e300cf45a76e671b7db3d65
星空吖 <@DESKTOP-2LM47VU>
2019-12-13 10:26:08 +0800

    1

---
+++ WBSModel/Tb_ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBSModel
+{
+    /// <summary>
+    /// 购物车
+    /// </summary>
+    public class Tb_ShoppingCart
+    {
+       public int ShoppingCartID      { get; set; }
+       public int GoodsID             { get; set; }
+       public int ShoppingCartNum     { get; set; }
+       public int MemberID            { get; set; }
+       public DateTime EvaluaTime { get; set; }
+    }
+}
---
+++ WBSModel/Tb_areaInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBSModel
+{
+    public class Tb_areaInfo
+    {
+     public int   AreaInfoID            {get; set; }
+     public string   AreaInfoName          {get; set; }
+     public int   AreaInfoOrder         {get; set; }
+     public int   AreaInfoFatherID      {get; set; }
+     public int   AreaInfoDefault       {get; set; }
+     public int AreaInfoState { get; set; }
+    }
+}
